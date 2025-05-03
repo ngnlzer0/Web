a623f56cc2c94c43736908f610fdbde966a56297
Midhight <levitskiyivan3@gmail.com>
2025-05-03 22:55:37 +0300

    add Labs for Web in github

---
+++ slider.js
@@ -0,0 +1,38 @@
+let slideIndex = 0;
+showSlides();
+showSlidesManual(slideIndex);
+
+function plusSlides(n) {
+    showSlidesManual(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlidesManual(slideIndex = n);
+}
+
+function showSlidesManual(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex - 1].style.display = "block";
+}
